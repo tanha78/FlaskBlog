29a27e19b5808d6ab27fc843532ce1022cf28a2c
tanah <aliashhar@gmail.com>
2020-05-16 22:10:06 +0430

    Seprate views by __init__.py

--- mod_admin/__init__.py
+++ mod_admin/__init__.py
@@ -1,9 +1,7 @@
 from flask import Blueprint
 
 
-admin = Blueprint('admin', __name__, url_prefix='/admin')
+admin = Blueprint('admin', __name__, url_prefix='/admin/')
 
 
-@admin.route('/')
-def amin_index():
-    return 'Hello from admin index '
+from .views import index 
