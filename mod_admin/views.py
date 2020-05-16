95fa99260399ab19002976a2c017366212460b66
tanah <aliashhar@gmail.com>
2020-05-16 22:11:34 +0430

    sessions initialize admin by view

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,6 +1,16 @@
+from flask import session
 from . import admin  
 
 
 @admin.route('/')
 def index():
     return 'Hello from admin index '
+
+
+@admin.route('/login/')
+def login():
+    session['name'] = 'Ali'
+    #session.clear()
+    #print(session.get('name'))
+    print(session)
+    return '1'
